cb59dfb95e33b36b4b3c324eb63100c05d63a33d
Toriola <faaedah@gmail.com>
2023-12-08 11:18:43 +0100

    REFACTOR: changed navbar in layout page

--- src/components/layout/CommonPageLayout.jsx
+++ src/components/layout/CommonPageLayout.jsx
@@ -1,13 +1,19 @@
-import NavBar from "../shared/NavBar";
 import { ScrollRestoration } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function CommonPageLayout({ children, title, heroImg }) {
    return (
       <>
-         <NavBar />
-         <ScrollRestoration/>
-         <main>
-            <div className="max-w-[700px] mx-auto my-32 px-4">
+         <ScrollRestoration />
+         <nav className="border-b fixed top-0 right-0 z-50 left-0 bg-white py-6">
+            <div className="relative max-w-[1200px] mx-auto">
+               <Link to="/" className="text-2xl">
+                  TC
+               </Link>
+            </div>
+         </nav>
+         <div className="max-w-[700px] mx-auto my-32 px-4">
+            <header>
                <h1 className="text-dark font-semibold text-2xl md:text-3xl">
                   {title}
                </h1>
@@ -17,9 +23,11 @@ function CommonPageLayout({ children, title, heroImg }) {
                   role="presentation"
                   className="h-[200px] md:h-[300px] w-full object-cover my-10"
                />
-               {children}
-            </div>
-         </main>
+            </header>
+            <main>
+               <div>{children}</div>
+            </main>
+         </div>
       </>
    );
 }
